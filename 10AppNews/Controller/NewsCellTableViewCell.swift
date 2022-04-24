42c00e8daba9521e7d7d025bdd151ec3d9373481
djdenielb <luisdmateos@outlook.com>
2022-04-23 22:33:23 -0500

    Creado el diseño inicial de la celda y las pantallas

---
+++ 10AppNews/Controller/NewsCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsCellTableViewCell.swift
+//  10AppNews
+//
+//  Created by djdenielb on 23/04/22.
+//
+
+import UIKit
+
+class NewsCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
